3d2caaa2d22b7d09dafde5ac6b14204349cb8e5d
kuckjwi <rnrghks09@gmail.com>
2021-12-12 18:58:07 +0900

    add chapter4 exercise 04-1

---
+++ chapter04/exercise-04-1/exercise.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+class FuritSeller
+{
+    private:
+        int APPLE_PRICE;
+        int numOfApples;
+        int myMoney;
+    public:
+        void InitMembers(int price, int num, int money)
+        {
+            APPLE_PRICE = price;
+            numOfApples = num;
+            myMoney = money;
+        }
+        int SaleApples(int money)
+        {
+            int num = money / APPLE_PRICE;
+            numOfApples -= num;
+            myMoney += money;
+            return num;
+        }
+        void ShowSaleResult() const
+        {
+            cout<<"남은사과: "<<numOfApples<<endl;
+            cout<<"판매수익: "<<myMoney<<endl;
+        }
+};
+
+class FruitBuyer
+{
+    int myMoney;
+    int numOfApples;
+
+    public:
+        void InitMembers(int money)
+        {
+            if (money <= 0)
+            {
+                throw std::invalid_argument("money must be greater than zero!");
+            }
+            myMoney = money;
+            numOfApples = 0;
+        }
+        void BuyApples(FuritSeller &seller, int money)
+        {
+            numOfApples += seller.SaleApples(money);
+            myMoney -= money;
+        }
+        void ShowBuyResult() const
+        {
+            cout<<"현재 잔액: "<<myMoney<<endl;
+            cout<<"사과 개수: "<<numOfApples<<endl;
+        }
+};
+
+int main(void)
+{
+    FuritSeller seller;
+    seller.InitMembers(1000, 20, 0);
+    FruitBuyer buyer;
+    buyer.InitMembers(5000);
+    buyer.BuyApples(seller, 2000);
+    cout<<"Seller"<<endl;
+    seller.ShowSaleResult();
+    cout<<"Buyer"<<endl;
+    buyer.ShowBuyResult();
+}

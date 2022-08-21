7206c0bd4b741c901f53380822575a38f1be3c27
kuckjwi <rnrghks09@gmail.com>
2022-08-21 17:27:33 +0900

    add inherit example

---
+++ chapter07/UnivStudentlnheri.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Person
+{
+private:
+    int age;
+    char name[50];
+public:
+    Person(int myage, char *myname) : age(myage)
+    {
+        strcpy(name, myname);
+    }
+
+    void WhatYourName() const
+    {
+        cout << "My name is " << name << endl;
+    }
+
+    void HowOldAreYou() const
+    {
+        cout << "I'm " << age << " years old" << endl;
+    }
+};
+
+class UnivStudent : public Person
+{
+private:
+    char major[50];
+public:
+    UnivStudent(char *myname, int myage, char *mymajor) : Person(myage, myname)
+    {
+        strcpy(major, mymajor);
+    }
+
+    void WhoAreYou() const
+    {
+        WhatYourName();
+        HowOldAreYou();
+        cout << "My major is " << major << endl;
+    }
+};
+
+int main(void)
+{
+    UnivStudent ustd1("kuckhwan", 31, "Computer eng.");
+    ustd1.WhoAreYou();
+
+    return 0;
+}

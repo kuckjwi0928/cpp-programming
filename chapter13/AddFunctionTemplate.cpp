03109c4da723b84507c71a917a831b324e3ca2bc
kuckjwi <rnrghks09@gmail.com>
2023-01-13 17:16:19 +0900

    add template example

---
+++ chapter13/AddFunctionTemplate.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+T Add(T num1, T num2)
+{
+    return num1 + num2;
+}
+
+int main(void)
+{
+    cout << Add<int>(15, 20) << endl;
+    cout << Add<double>(2.9, 3.7) << endl;
+
+    return 0;
+}
